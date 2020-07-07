16ba6524f59028ec76b11d9cf858b6f5efbe0c00
xiongzihaoc <xiongzihao1997@gmail.com>
2020-07-07 10:59:30 +0800

    优化

--- src/components/BasicInformation/DoctorInfo.vue
+++ src/components/BasicInformation/DoctorInfo.vue
@@ -410,10 +410,10 @@ export default {
       },
       addTagRules: {
         specialityValue: [
-          { required: true, message: "请输入用户名", trigger: "blur" }
+          { required: true, message: "请输入标签", trigger: "blur" }
         ],
         specialityColor: [
-          { required: true, message: "请输入密码", trigger: "blur" }
+          { required: true, message: "请添加颜色", trigger: "blur" }
         ]
       },
       tagDialogVisible: false,
